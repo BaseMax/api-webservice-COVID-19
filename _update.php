396b4fa58c96f8cbe27d06217e8009e60af46be0
Max Base <Max@Asrez.com>
2020-02-27 18:52:29 +0330

    Add _update.php file and main script to crawler...

---
+++ _update.php
@@ -0,0 +1,68 @@
+<?php
+if(!defined('BASE')) {
+	define('BASE', __DIR__ . '/');
+}
+require_once "_core.php";
+require_once "_api.php";
+$tmpFileName="page.html";
+// Crontab command: `* */2 * * * php _update.php >/dev/null 2>&1`
+// Current time is: 2020-02-26 7:29:00 PM UTC
+// This cron job will be run at: (5 times displayed and more...)
+// 2020-02-26 20:00:00 UTC
+// 2020-02-26 20:01:00 UTC
+// 2020-02-26 20:02:00 UTC
+// 2020-02-26 20:03:00 UTC
+// 2020-02-26 20:04:00 UTC
+// ...
+function update() {
+	global $db;
+	global $tmpFileName;
+	$file=get("https://www.worldometers.info/coronavirus/");
+	// print_r($file);
+	$file=$file[0];
+	// file_put_contents is suitable for here, but we will use curl for other projects!
+	// Sample library and project in this case: https://github.com/BaseMax/netphp
+	file_put_contents($tmpFileName, $file);
+	$matchs=parseData($file);
+	$items=prepareData($matchs);
+	// print_r($items);
+	foreach($items as $item) {
+		// name, totalCase, newCase, totalDeath, newDeath, totalRecovered, seriousUser
+		$clauses=[
+			"name"=>$item["name"],
+		];
+		// print_r($item);
+		foreach($item as $i=>$v) {
+			if($i != "name") {
+				$item[$i]=str_replace(",", "", $item[$i]);// ,
+				$item[$i]=str_replace("+", "", $item[$i]);// +
+			}
+		}
+		print_r($item);
+		$values=[
+			"name"=>$item["name"],
+			"totalCase"=>$item["totalCase"],
+			"newCase"=>$item["newCase"],
+			"totalDeath"=>$item["totalDeath"],
+			"newDeath"=>$item["newDeath"],
+			"totalRecovered"=>$item["totalRecovered"],
+			"seriousUser"=>$item["seriousUser"],
+		];
+		if($db->count("country", $clauses) == 0) {
+			$db->insert("country", $values);
+		}
+		else {
+			if($db->count("country", $values) == 0) {
+				// skip
+			}
+			else {
+				print "Update...\n";
+				$db->update("country", $clauses, $values);
+			}
+		}
+	}
+}
+if(isset($argv) and isset($argv[1]) and $argv[1] == "update") {
+	print "Update...\n";
+	update();
+}
