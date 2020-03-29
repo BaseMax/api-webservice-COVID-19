456b9bf612a3d546fe45a02658c9bee5f6b53827
Max Base <maxbasecode@gmail.com>
2020-03-29 17:25:50 +0430

    Add test file and fix new bug

--- src/_core.php
+++ src/_core.php
@@ -4,7 +4,7 @@ if(!defined("BASE")) {
 }
 require_once "_phpedb.php";
 $db=new database();
-$db->connect("localhost", "root", "***");
+$db->connect("localhost", "root", "01");
 $db->db="corona";
 $db->create_database($db->db, false);
 function display($array) {
